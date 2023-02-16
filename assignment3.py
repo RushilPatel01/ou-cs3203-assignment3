88a749104d9d5d80c59dfb6f127814b577cf1960
RushilPatel01 <89539872+RushilPatel01@users.noreply.github.com>
2023-02-16 17:56:31 -0600

    Added main method.

--- assignment3.py
+++ assignment3.py
@@ -10,8 +10,24 @@ def sum_Of_List(number_List):
 def product_Of_List(number_List):
 
     product = number_List[0]
-    
+
     for number in number_List[1:]:
         product = product * number
 
     return product
+
+if __name__ == '__main__':
+
+    list_Of_Numbers = []
+
+    print('Press "Enter" after each number. Type "Done" when finished.')
+
+    while True:
+        add_To_List = input()
+        if add_To_List == "Done":
+            break
+        list_Of_Numbers.append(int(add_To_List))
+
+    print(f"Sum of list: {sum_Of_List(list_Of_Numbers)}")
+    
+    print(f"Product of list: {product_Of_List(list_Of_Numbers)}")
