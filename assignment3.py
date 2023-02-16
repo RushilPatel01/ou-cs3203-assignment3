22459ec06bc4f416d6dc8b1b1ca0902d33174637
RushilPatel01 <89539872+RushilPatel01@users.noreply.github.com>
2023-02-16 17:54:07 -0600

    Added function that computes the product of a list.

--- assignment3.py
+++ assignment3.py
@@ -6,3 +6,12 @@ def sum_Of_List(number_List):
         sum = sum + number
 
     return sum
+
+def product_Of_List(number_List):
+
+    product = number_List[0]
+    
+    for number in number_List[1:]:
+        product = product * number
+
+    return product
